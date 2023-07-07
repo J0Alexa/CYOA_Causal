d191c6d8c11af373dd802731e12ee3ce40887a4f
J0Alexa <138896797+J0Alexa@users.noreply.github.com>
2023-07-07 11:46:27 -0400

    Update causal_ctrl.js

--- causal_august/causal_ctrl.js
+++ causal_august/causal_ctrl.js
@@ -4,7 +4,7 @@
 **********************************/
 
 //GLOBAL VARIABLES
-var mycounterbalance = Math.floor(Math.random() * 2) + 1; //have 16 subjects' story version left to collect causal rating on 
+var mycounterbalance = Math.floor(Math.random() * 3) + 1; //have 16 subjects' story version left to collect causal rating on 
 
 // set route_vector and choice_vector variables
 if (mycounterbalance == 1) {
@@ -12,6 +12,9 @@ if (mycounterbalance == 1) {
   var link = 'https://jh.qualtrics.com/jfe/form/SV_doqxCA7AUcLjLIa' 
 } else if (mycounterbalance == 2) {
   var route_subject= "sub79_7079"
+  var link = 'https://jh.qualtrics.com/jfe/form/SV_783nNHRQIhxGK7Y' 
+} else if (mycounterbalance == 3) {
+  var route_subject= "sub5_7005"
   var link = 'https://jh.qualtrics.com/jfe/form/SV_783nNHRQIhxGK7Y' 
 };
 
