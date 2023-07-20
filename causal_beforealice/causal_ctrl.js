b35fb938ee4f01b87040fd0e64990f4dbd7b9028
J0Alexa <138896797+J0Alexa@users.noreply.github.com>
2023-07-19 21:34:48 -0400

    Update causal_ctrl.js

--- causal_beforealice/causal_ctrl.js
+++ causal_beforealice/causal_ctrl.js
@@ -9,13 +9,13 @@ var mycounterbalance = Math.floor(Math.random() * 3) + 1; //have 16 subjects' st
 // set route_vector and choice_vector variables
 if (mycounterbalance == 1) {
   var route_subject= "sub6_1005"
-  var link = 'https://jh.qualtrics.com/jfe/form/SV_eypHd5TieSzrrfwa' 
+  var link = 'https://jh.qualtrics.com/jfe/form/SV_eypHd5TieSzrrfw' 
 } else if (mycounterbalance == 2) {
   var route_subject= "sub12_1011"
-  var link = 'https://jh.qualtrics.com/jfe/form/SV_1HMX4EWjm0CXKt0' 
+  var link = 'https://jh.qualtrics.com/jfe/form/SV_7O0j68Zirak3GB0' 
 } else if (mycounterbalance == 3) {
   var route_subject= "sub3_1002"
-  var link = 'https://jh.qualtrics.com/jfe/form/SV_b8HATMrTQOs4qJo' 
+  var link = 'https://jh.qualtrics.com/jfe/form/SV_a8HcT0P0rr95SHY' 
 };
 
 
