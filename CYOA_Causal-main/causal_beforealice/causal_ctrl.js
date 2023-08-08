8b1c614cec12d7f657ffceb481082488a75b11de
J0Alexa <138896797+J0Alexa@users.noreply.github.com>
2023-08-07 22:09:00 -0400

    Update causal_ctrl.js

--- CYOA_Causal-main/causal_beforealice/causal_ctrl.js
+++ CYOA_Causal-main/causal_beforealice/causal_ctrl.js
@@ -4,18 +4,15 @@
 **********************************/
 
 //GLOBAL VARIABLES
-var mycounterbalance = Math.floor(Math.random() * 3) + 1; //have 16 subjects' story version left to collect causal rating on 
+var mycounterbalance = Math.floor(Math.random() * 2) + 1; //have 16 subjects' story version left to collect causal rating on 
 
 // set route_vector and choice_vector variables
 if (mycounterbalance == 1) {
-  var route_subject= "sub55_7055"
-  var link = 'https://jh.qualtrics.com/jfe/form/SV_doqxCA7AUcLjLIa' 
+  var route_subject= "sub21_1020"
+  var link = 'https://jh.qualtrics.com/jfe/form/SV_6hCaDD6V95uhB0W' 
 } else if (mycounterbalance == 2) {
-  var route_subject= "sub79_7079"
-  var link = 'https://jh.qualtrics.com/jfe/form/SV_783nNHRQIhxGK7Y' 
-} else if (mycounterbalance == 3) {
-  var route_subject= "sub5_7005"
-  var link = 'https://jh.qualtrics.com/jfe/form/SV_bOtJYoh5lEnShDg' 
+  var route_subject= "sub22_1021"
+  var link = 'https://jh.qualtrics.com/jfe/form/SV_0PB41KkaHUzrdky' 
 };
 
 
